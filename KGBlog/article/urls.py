179bcc69a7141c03ae411bfd1c7863a91e7b9341
Kevin Gleason <kaagleason@gmail.com>
2014-04-09 16:38:26 -0400

    Moving directories

---
+++ KGBlog/article/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, url
+
+from article import views
+
+urlpatterns = patterns('',
+	url(r'^$', views.articles),
+	url(r'^all/$', views.articles),
+	url(r'^get/(?P<article_id>\d+)/$', views.article),
+
+	## Hello URLS
+	url(r'^hello/$', views.hello, name = 'index'),
+    url(r'^helloT/$', views.helloWithTemp, name='HelloT'),
+    url(r'^helloC/$',  views.HelloTemplate.as_view(), name='HelloC'),
+    url(r'^language/(?P<language>[a-z\-]+)$', views.language, name='langSet'),
+)
